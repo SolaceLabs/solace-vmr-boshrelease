0f2a940cdee1e7a6dbdbc6976a77c7ee8fe94fc2
Francois Dickey <francois.dickey@solacesystems.com>
2016-11-07 21:37:54 +0000

    Adding community docker image to solace-vmr blobs.

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ mkdir -p blobs/soltr_docker
 tar czf blobs/vmr_config_scripts/vmr_config_scripts.tgz src/vmr_config_scripts/*
 
 if [ -d /home/public/RND/loads ]; then
-    cp -f `ls /home/public/RND/loads/solcbr/$SOLTR_LOAD/production/*evaluation-docker.tar.gz` blobs/soltr_docker/soltr-docker.tgz
+    ruby ./buildUtils/imagePacker.rb `ls /home/public/RND/loads/solcbr/$SOLTR_LOAD/production/*evaluation-docker.tar.gz` `ls /home/public/RND/loads/solcbr/$SOLTR_LOAD/production/*community-docker.tar.gz` > blobs/soltr_docker/soltr-docker.tgz
 else
     # Convenience for people working with bosh-lite.  Drop the latest container tar.gz in the host's vagrant folder and
     # the script will move it into the build's blobs.
     if [ -f /vagrant/*evaluation-docker.tar.gz ]; then
-        mv -f `ls /vagrant/*evaluation-docker.tar.gz` blobs/soltr_docker/soltr-docker.tgz
+        ruby ./buildUtils/imagePacker.rb `ls /vagrant/*evaluation-docker.tar.gz` `ls /vagrant/*community-docker.tar.gz` blobs/soltr_docker/soltr-docker.tgz
     fi
 fi
 
